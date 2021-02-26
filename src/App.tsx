06df7f4374479b9f1827fbac25cbbd7139056a06
Joseph Carrington <jcarrington@meowwolf.com>
2021-02-26 09:59:56 -0700

    defaulted

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,20 @@
 import React, { FunctionComponent, useContext, useEffect } from 'react'
-import { Platform, PlatformInterfaceContext, GameStateContext } from '@meowwolf/react-platform-connection'
-
-import { IInventoryItem } from './typings'
+import { Platform, ConfigContext } from '@meowwolf/react-platform-connection'
 
 const App: FunctionComponent = () => {
   return (
     <Platform>
-      <Ossilator />
+      <Tester />
     </Platform>
   )
 }
 
-const Ossilator: FunctionComponent = () => {
-  const gameState = useContext(GameStateContext)
-  const platformInterface = useContext(PlatformInterfaceContext)
-
+const Tester: FunctionComponent = () => {
+  const config = useContext(ConfigContext)
   useEffect(() => {
-    if (!gameState || !platformInterface || !gameState.user.properties.inventory) return
-    if (gameState.user.properties.ossButtonsPressed) {
-      const { upsertUserProperties } = platformInterface
-      upsertUserProperties({
-        uuid: gameState.user.uuid,
-        properties: {
-          ossButtonsPressed: false,
-          inventory: {
-            items: [
-              ...gameState.user.properties.inventory.items.filter(
-                (item: IInventoryItem) =>
-                  item.title !== 'Depamina 1:3' &&
-                  item.title !== 'Depamina 2:3' &&
-                  item.title !== 'Depamina 3:3' &&
-                  item.title !== 'Defragmented Depamina Memory',
-              ),
-              {
-                title: 'Defragmented Depamina Memory',
-                quantity: 1,
-                icon: {
-                  source: 'http://10.4.48.188:4001/icons/depamina-full.png',
-                  name: 'depamina-full.png',
-                },
-                description: "This is Depamina's defragmented memory.",
-              },
-            ],
-          },
-        },
-      })
-      window.location.reload()
-    }
-  }, [gameState, platformInterface])
-
-  return gameState && platformInterface ? (
-    <div>User booped, but does not have ossButtonsPressed</div>
-  ) : (
-    <div>Waiting for boops</div>
-  )
+    console.log(config)
+  }, [config])
+  return <p>Loaded Config. Things seem good to go.</p>
 }
 
 export default App
