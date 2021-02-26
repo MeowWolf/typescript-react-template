16fb23c1634c5486a821dfe325efcf6c35c523c3
Joseph Carrington <jcarrington@meowwolf.com>
2021-02-26 10:38:44 -0700

    Properly use env vars

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,24 @@ import React, { FunctionComponent, useContext, useEffect } from 'react'
 import { Platform, ConfigContext } from '@meowwolf/react-platform-connection'
 
 const App: FunctionComponent = () => {
+  if (
+    !process.env.REACT_APP_MESSAGE_BUS_LOCATION ||
+    !process.env.REACT_APP_PLATFORM_LOCATION ||
+    !process.env.REACT_APP_CORE_API_PORT ||
+    !process.env.REACT_APP_MESSAGE_BUS_PORT ||
+    !process.env.REACT_APP_PLATFORM_TOKEN ||
+    !process.env.REACT_APP_CORE_API_PORT
+  )
+    return null
+  console.log(process.env)
   return (
-    <Platform>
+    <Platform
+      messageBusPort={parseInt(process.env.REACT_APP_MESSAGE_BUS_PORT)}
+      messageBusLocation={process.env.REACT_APP_MESSAGE_BUS_LOCATION}
+      platformToken={process.env.REACT_APP_PLATFORM_TOKEN}
+      platformIP={process.env.REACT_APP_PLATFORM_LOCATION}
+      coreAPIPort={parseInt(process.env.REACT_APP_CORE_API_PORT)}
+    >
       <Tester />
     </Platform>
   )
